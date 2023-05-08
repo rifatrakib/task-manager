c009fbc98a001da2d9aa9dfbd9f8eafe4d46111f
rifatrakib <Rakib.1508@outlook.com>
2023-05-08 22:15:48 +0600

    add root type for handling phone numbers

--- server/manager/types.py
+++ server/manager/types.py
@@ -1,7 +1,10 @@
+import re
 from datetime import datetime
 from typing import Callable, Generator, Union
 from uuid import UUID
 
+import phonenumbers
+from phonenumbers.phonenumberutil import NumberParseException
 from pydantic.datetime_parse import parse_datetime
 
 from server.manager.utils import as_utc, get_timestamp, get_utc_timezone
@@ -52,3 +55,28 @@ class Timestamp(float):
     def __modify_schema__(cls, field_schema: dict) -> None:
         """Update type OpenAPIv3 schema."""
         field_schema.update(example=1656080975146.785, examples=[1656080947257.345, 1656080975146])
+
+
+class Phone(str):
+    @classmethod
+    def __get_validators__(cls) -> Generator[Callable[[str], str], None, None]:
+        """Run validate class method."""
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v: str) -> str:
+        prefix = "+"
+        if not re.match(r"^\d{8,15}$", v):
+            raise ValueError("Must be digits")
+
+        try:
+            v = "".join((digit for digit in v if digit.isdigit()))  # format phone (allow only digits)
+            v = prefix + v
+            parsed_phone = phonenumbers.parse(v, None)
+        except NumberParseException as error:
+            raise ValueError("Invalid phone number") from error
+        else:
+            if phonenumbers.is_possible_number(parsed_phone):
+                return v.removeprefix(prefix)
+
+        raise ValueError("Impossible Number")
